95a16c5b54709fb1d8aedfa0f9a91290e8ee0f01
Lukas <lucasbarrientos7@hotmail.com>
2021-08-04 22:49:12 -0300

    disparo barra

--- SpaceshipBRRII.cpp
+++ SpaceshipBRRII.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define IZQUIERDA 75
 #define DERECHA 77
 #define ABAJO 80
+#define BARRA 32
 
 void gotoxy(int x, int y)
 {
@@ -121,7 +122,7 @@ void NAVE::pintar_corazones()
     gotoxy(50, 2);
     printf("LIFE %d", vidas);
     gotoxy(64, 2);
-    printf("Health ");
+    printf("Health");
     gotoxy(70, 2);
     printf("      ");
     for (int i = 0; i < corazones; i++)
@@ -262,7 +263,7 @@ int main()
         if (_kbhit())
         {
             char tecla = _getch();
-            if (tecla == 'a')
+            if (tecla == BARRA)
                 B.push_back(new BALA(N.X() + 2, N.Y() - 1));
         }
         // <-Correcion del error
